7b01d4bd1f85ac5e415bef94dc616c856ac4b634
kamikami8487 <kamikami8487@gmail.com>
2023-10-09 01:21:19 +0500

    revert

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: true,
-  API_BASE_URL: 'http://203.99.177.6/web-api',
+  API_BASE_URL: 'http://203.99.177.6:81',
   JASPER_BASE_URL: 'http://203.99.177.6:8080',
 
 };
-
